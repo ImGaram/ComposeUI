5154fcd9336e9dd8b168836689ea296640723bbf
임가람 <84944117+ImGaram@users.noreply.github.com>
2022-11-30 23:35:16 +0900

    gradle 설정

--- app/src/main/java/com/example/composeui/MainActivity.kt
+++ app/src/main/java/com/example/composeui/MainActivity.kt
@@ -1,14 +1,14 @@
 package com.example.composeui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.composeui.ui.theme.ComposeUITheme
 
@@ -27,11 +27,13 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+// 뷰
 @Composable
 fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+// 뷰를 그리는 곳(미리보기)
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
