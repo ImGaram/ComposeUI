062cf9500ad3d353aa66c363d82795fed5791ce6
임가람 <84944117+ImGaram@users.noreply.github.com>
2022-12-01 17:11:45 +0900

    android compose 연습 1

--- app/src/main/java/com/example/composeui/MainActivity.kt
+++ app/src/main/java/com/example/composeui/MainActivity.kt
@@ -4,12 +4,14 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.composeui.ui.theme.ComposeUITheme
 
 class MainActivity : ComponentActivity() {
@@ -27,17 +29,28 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-// 뷰
+// 텍스트 뷰
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Text(text = name)
 }
 
 // 뷰를 그리는 곳(미리보기)
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    ComposeUITheme {
-        Greeting("Android")
+    ComposeUITheme {    // 자동으로 생성되는 테마
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp),
+        ) {
+            Column(
+                modifier = Modifier.padding(all = 10.dp)
+            ) {
+                Text(text = "Hello Android!")
+                Text(text = "Content!")
+            }
+        }
     }
 }
